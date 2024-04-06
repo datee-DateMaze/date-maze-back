a37339e250c8a611765eeb6bf93f490f58cc8a08
qbobl5 <ddddd991125@gmail.com>
2024-04-07 02:15:45 +0900

    fix: couple entity 수정

--- src/main/java/com/datee/datemaze/couple/entity/Couple.java
+++ src/main/java/com/datee/datemaze/couple/entity/Couple.java
@@ -1,15 +1,14 @@
 package com.datee.datemaze.couple.entity;
 
+import com.datee.datemaze.calendar.entity.Calendar;
+import com.datee.datemaze.member.entity.Member;
 import com.datee.datemaze.util.BaseEntity;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
@@ -23,29 +22,28 @@ import java.util.Date;
 @Table(name = "couple")
 @DynamicInsert
 @DynamicUpdate
-/* 커플 엔티티 */
+/* 커플 entity */
 public class Couple extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "couple_no")
-    @NotNull
     private Long coupleNo; // 커플 번호
 
     @Column(name = "couple_name")
     private String coupleName; // 커플명
 
-    @Column(name = "member1_no")
-    @NotNull
-    private Long member1No; // 회원1 번호
+    @ManyToOne
+    @JoinColumn(name = "member1_no")
+    private Member member1; // 회원1
 
-    @Column(name = "member2_no")
-    @NotNull
-    private Long member2No; // 회원2 번호
+    @ManyToOne
+    @JoinColumn(name = "member2_no")
+    private Member member2; // 회원2
 
-    //    @Column(name = "cal_no")
-    //    @NotNull
-    //    private Long calNo; // 캘린더 번호
+    @OneToOne(mappedBy = "couple", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "cal_no")
+    private Calendar calendar; // 캘린더 번호
 
     @Column(name = "couple_date")
     @NotNull
