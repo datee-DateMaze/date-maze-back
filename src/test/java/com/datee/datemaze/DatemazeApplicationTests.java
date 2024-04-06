3f5339090a683b6a599bc4ea5828f3eb3b09f24e
jsjhyun <gusrnl98@naver.com>
2024-04-06 19:18:58 +0900

    fix: springboottest class 지정

--- src/test/java/com/datee/datemaze/DatemazeApplicationTests.java
+++ src/test/java/com/datee/datemaze/DatemazeApplicationTests.java
@@ -3,9 +3,10 @@ package com.datee.datemaze;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DatemazeApplicationTests.class)
 class DatemazeApplicationTests {
 
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+    }
 }
