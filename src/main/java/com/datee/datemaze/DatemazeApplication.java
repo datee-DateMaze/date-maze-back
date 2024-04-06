84b232b9c2c1fb5cea776d2a59fbaa7eaa51eca8
qbobl5 <ddddd991125@gmail.com>
2024-04-06 17:07:13 +0900

    project upload

---
+++ src/main/java/com/datee/datemaze/DatemazeApplication.java
@@ -0,0 +1,13 @@
+package com.datee.datemaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatemazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatemazeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datee/datemaze/DatemazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datee.datemaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatemazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
