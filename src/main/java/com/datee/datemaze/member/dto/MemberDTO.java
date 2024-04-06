c237db8bcca3544ddb6a30618bea5f40ee9752df
qbobl5 <ddddd991125@gmail.com>
2024-04-06 17:56:26 +0900

    feat: git action test

---
+++ src/main/java/com/datee/datemaze/member/dto/MemberDTO.java
@@ -0,0 +1,12 @@
+package com.datee.datemaze.member.dto;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDTO {
+    private Long memberNo;
+}
